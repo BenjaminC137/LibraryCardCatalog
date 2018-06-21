a7a0d9449ba81445d37cc2b89665e134c04ff68e
Benjamin <benjaminsalasek@Athena.local>
2018-06-21 15:21:31 -0500

    Fix books class

---
+++ LibraryCardCatalog/Books.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LibraryCardCatalog
+{
+    public class Books
+    {
+            public string Name { get; set; }
+            public string Author { get; set; }
+            public string Genre { get; set; }
+            public string ISBN { get; set; }
+
+    }
+}
