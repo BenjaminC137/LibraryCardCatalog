0c0b77ffacafa754548c237d6b4658093838e217
Benjamin <benjaminsalasek@Athena.local>
2018-06-22 10:37:06 -0500

    trying write to file

--- LibraryCardCatalog/CardCatalog.cs
+++ LibraryCardCatalog/CardCatalog.cs
@@ -8,6 +8,64 @@ namespace LibraryCardCatalog
 {
     public class CardCatalog
     {
+        List<Books> booksList = new List<Books>();
 
+        public void CardCatalogMethod()
+        {
+            bool exit = false;
+            while (exit == false)
+            {
+                string selectionPrompt = "Type '1' to list all books." +
+                Environment.NewLine + "Type '2' to add a book." +
+                Environment.NewLine + "Type '3' to save and exit.";
+
+                Console.WriteLine(selectionPrompt);
+                string userSelection = Console.ReadLine();
+
+                if (userSelection == "1")
+                {
+                    Console.WriteLine("Here are the books!");
+                    //use serialization here
+                }
+
+                if (userSelection == "2")
+                {
+                    AddBook();
+                }
+
+                if (userSelection == "3")
+                {
+                    exit = true;
+                    Console.WriteLine("exiting program...");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        void AddBook()
+        {
+            Console.WriteLine("Enter Book ISBN");
+            string isbnEntry = Console.ReadLine();
+
+            Console.WriteLine("Enter Title");
+            string titleEntry = Console.ReadLine();
+
+            Console.WriteLine("Enter Author");
+            string authorEntry = Console.ReadLine();
+
+            Console.WriteLine("Enter Genre");
+            string genreEntry = Console.ReadLine();
+
+            booksList.Add(new Books(titleEntry, authorEntry, genreEntry, isbnEntry));
+          
+
+            foreach (var book in booksList)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+        }
+
+        private string _filename;
     }
 }
--- LibraryCardCatalog/Program.cs
+++ LibraryCardCatalog/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Xml.Serialization;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace LibraryCardCatalog
 {
@@ -10,68 +12,17 @@ namespace LibraryCardCatalog
         {
             Console.WriteLine("Hello. Please enter a file name.");
 
-            Books b1 = new Books("Dictionary", "Oxford", "Education", "0001");
+            string fileName = Console.ReadLine();
 
-            List<Books> booksList = new List<Books>();
+            File.Create(c:\LibraryCatalog\fileName.txt);
 
-            CardCatalogMethod();
+            //Books b1 = new Books("Dictionary", "Oxford", "Education", "0001");
 
-            void CardCatalogMethod()
-            {
-                bool exit = false;
-                while (exit == false)
-                {
-                    string selectionPrompt = "Type '1' to list all books." +
-                    Environment.NewLine + "Type '2' to add a book." +
-                    Environment.NewLine + "Type '3' to save and exit.";
+            List<Books> booksList = new List<Books>(); // create list to store books in
 
-                    Console.WriteLine(selectionPrompt);
-                    string userSelection = Console.ReadLine();
+            CardCatalog cc = new CardCatalog();
+            cc.CardCatalogMethod();
 
-                    if (userSelection == "1")
-                    {
-                        Console.WriteLine("Here are the books!");
-                        //use serialization here
-                    }
-
-                    if (userSelection == "2")
-                    {
-                        AddBook();
-                    }
-
-                    if (userSelection == "3")
-                    {
-                        exit = true;
-                        Console.WriteLine("exiting program...");
-                        Console.ReadLine();
-                    }
-                }
-            }
-
-            void AddBook()
-            {
-                Console.WriteLine("Enter Book ISBN");
-                string isbnEntry = Console.ReadLine();
-
-                Console.WriteLine("Enter Title");
-                string titleEntry = Console.ReadLine();
-
-                Console.WriteLine("Enter Author");
-                string authorEntry = Console.ReadLine();
-
-                Console.WriteLine("Enter Genre");
-                string genreEntry = Console.ReadLine();
-
-                booksList.Add(new Books(titleEntry, authorEntry, genreEntry, isbnEntry));
-
-                Console.WriteLine();
-                foreach (var book in booksList)
-                {
-                    Console.WriteLine("Title: {0} | Author: {1}", book.Title, book.Author);
-                }
-                Console.ReadLine();
-            }
-            //My changes
         }
     }
 }
