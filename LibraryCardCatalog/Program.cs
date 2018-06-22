3d71cbba848cf3426d3bc392d3eab815671178b1
Benjamin <benjaminsalasek@Athena.local>
2018-06-21 20:59:07 -0500

    user entry results demo
    Configured to use as demo to test user prompts. Moved catalog class contents out of the class and into program class. created book list. Configured user entries to display. Created 'add book' method.

--- LibraryCardCatalog/Program.cs
+++ LibraryCardCatalog/Program.cs
@@ -1,22 +1,77 @@
 ﻿using System;
 using System.Xml.Serialization;
+using System.Collections.Generic;
 
 namespace LibraryCardCatalog
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
-
-            //This is my first push since initial push
+            Console.WriteLine("Hello. Please enter a file name.");
 
             Books b1 = new Books("Dictionary", "Oxford", "Education", "0001");
-            
 
+            List<Books> booksList = new List<Books>();
+
+            CardCatalogMethod();
+
+            void CardCatalogMethod()
+            {
+                bool exit = false;
+                while (exit == false)
+                {
+                    string selectionPrompt = "Type '1' to list all books." +
+                    Environment.NewLine + "Type '2' to add a book." +
+                    Environment.NewLine + "Type '3' to save and exit.";
+
+                    Console.WriteLine(selectionPrompt);
+                    string userSelection = Console.ReadLine();
+
+                    if (userSelection == "1")
+                    {
+                        Console.WriteLine("Here are the books!");
+                        //use serialization here
+                    }
+
+                    if (userSelection == "2")
+                    {
+                        AddBook();
+                    }
+
+                    if (userSelection == "3")
+                    {
+                        exit = true;
+                        Console.WriteLine("exiting program...");
+                        Console.ReadLine();
+                    }
+                }
+            }
+
+            void AddBook()
+            {
+                Console.WriteLine("Enter Book ISBN");
+                string isbnEntry = Console.ReadLine();
+
+                Console.WriteLine("Enter Title");
+                string titleEntry = Console.ReadLine();
+
+                Console.WriteLine("Enter Author");
+                string authorEntry = Console.ReadLine();
+
+                Console.WriteLine("Enter Genre");
+                string genreEntry = Console.ReadLine();
+
+                booksList.Add(new Books(titleEntry, authorEntry, genreEntry, isbnEntry));
+
+                Console.WriteLine();
+                foreach (var book in booksList)
+                {
+                    Console.WriteLine("Title: {0} | Author: {1}", book.Title, book.Author);
+                }
+                Console.ReadLine();
+            }
             //My changes
-
-
         }
     }
 }
